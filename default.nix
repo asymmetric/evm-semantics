42d31b7257c56adc07688ffe1081ff80e0c56893
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-09-28 17:10:39 +0200

    Use # as regex separator, to avoid conflicts

--- default.nix
+++ default.nix
@@ -32,10 +32,10 @@ in stdenv.mkDerivation rec {
   src = gitignore.gitignoreSourceAux additionalIgnores ./.;
 
   patchPhase = ''
-    sed -i 's|BUILD_DIR:=.*/\(.*\)$|BUILD_DIR:=$out/\1|' Makefile
-    sed -i 's|^K_BIN=.*$|K_BIN=${k}/bin|' Makefile
-    sed -i 's|^K_SUBMODULE:=.*$|K_SUBMODULE:=${k}|' Makefile
-    sed -i "s|-I .build/(java|haskell)$|-I $out/.build/\1|" Makefile
+    sed -i 's#BUILD_DIR:=.*/\(.*\)$#BUILD_DIR:=$out/\1#' Makefile
+    sed -i 's#^K_BIN=.*$#K_BIN=${k}/bin#' Makefile
+    sed -i 's#^K_SUBMODULE:=.*$#K_SUBMODULE:=${k}#' Makefile
+    sed -i "s#-I .build/(java|haskell)$#-I $out/.build/\1#" Makefile
   '';
 
   buildInputs = [ bison flex gmp git k makeWrapper ncurses opam-stub openjdk8 pandoc python3 ];
@@ -53,7 +53,7 @@ in stdenv.mkDerivation rec {
   '';
 
   fixupPhase = ''
-    sed -i 's|^build_dir=\(.*\)/|build_dir=\1/../|' $out/bin/kevm
+    sed -i 's#^build_dir=\(.*\)/#build_dir=\1/../#' $out/bin/kevm
 
     wrapProgram $out/bin/kevm \
       --prefix PATH : ${lib.makeBinPath [ k opam-stub openjdk8 z3 ]} \
