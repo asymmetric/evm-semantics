ab5f0b30db64c4363e11d2d863d95a84ae6e4549
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-10-26 18:17:09 +0200

    remove PWD: it was wrong

--- default.nix
+++ default.nix
@@ -58,7 +58,6 @@ in stdenv.mkDerivation rec {
   fixupPhase = ''
     set -x
     sed -i 's#^build_dir=\(.*\)/#build_dir=\1/../#' $out/bin/kevm
-    sed -i "s#kprove --directory \"\$build_dir/java/\"#kprove --directory \"$PWD\"#" $out/bin/kevm
 
     wrapProgram $out/bin/kevm \
       --prefix PATH : ${lib.makeBinPath [ k opam-stub openjdk8 z3 ]} \
